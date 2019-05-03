0990d22f1bad1c6554c8666bce84c31a2df6a7ac
Leandro Inacio <le.inacio.souza@gmail.com>
2019-05-02 18:28:19 -0700

    Working on it.

--- scripts/quizz.js
+++ scripts/quizz.js
@@ -1,24 +1,43 @@
-const question = document.getElementById("question");
-const choices = Array.from(document.getElementsByClassName("choice-text"));
+(function() {
+  const question = document.getElementById("question");
+  const choices = Array.from(document.getElementsByClassName("choice-text"));
 
-let currentQuestion;
-let isAvailable = false;
-let questions = [];
-let score = 0;
-let questionCounter = 0;
+  let currentQuestion;
+  let isAvailable = false;
+  let questions = [
+    {
+      question: "What is the brazilian oficial language?",
+      options: ["Spanish", "Portuguese", "English", "Chinese"],
+      correctAnswer: 1
+    }
+  ];
+  let score = 0;
+  let questionCounter = 0;
 
-// Constants
-const CORRECT_BONUT = 10;
-const MAX_QUESTIONS = 3;
+  // Constants
+  const CORRECT_BONUT = 10;
+  const MAX_QUESTIONS = 3;
 
-startGame = () => {
-  currentQuestion = 0;
-  isAvaiable = true;
-  score = 0;
-  questionCounter = 0;
-  console.log(`ok`);
-};
+  startGame = () => {
+    currentQuestion = 0;
+    isAvaiable = true;
+    score = 0;
+    questionCounter = 0;
+    availableQuestions = [...questions];
+    getNewQuestion();
+  };
 
-getNewQuestion = () => {};
+  getNewQuestion = () => {
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * questions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
 
-startGame();
+    choices.map(choice => {
+      const number = choice.dataset["number"];
+      choice.innerText = currentQuestion.options[number];
+    });
+  };
+
+  startGame();
+})();
